5e32c07faef7cb2b659a3e4b74db04b2bf7ec0fc
spenclark <spenclark@hotmail.com>
2020-05-14 15:24:55 -0700

    created util folder

---
+++ graph.py
@@ -0,0 +1,17 @@
+from utils import Queue, Stack
+# Graph Class 4 adv.py
+
+# implent graph with init (self.vertitices) and a df & bf traversal
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    
+    def dft(self, starting_vertex):
+        # declare set 
+        # init with starting vtex
+        # 
+        return None
+
+    def bfs(self, starting_vertex, destination_vertex):
+        q = Queue()
